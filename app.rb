028db1fdca720f9b8d0551240dfd56b30f8d5071
Ginny <ginnynjon@gmail.com>
2021-11-02 16:35:16 +0000

    updated app.rb & config.ru files to match walk through

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
-require 'sinatra'
+require 'sinatra/base'
 require "sinatra/reloader" 
 
-get '/Battle' do
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+get '/' do
   "Hello Battle!"
+end
+
+run! if app_file ==$0
+
 end
