1908ebc2327de52f373cd8f91270618514a69694
Myoung <mhbae93@gmail.com>
2021-11-06 01:01:38 +0900

    trying to implament changing players

--- lib/game.rb
+++ lib/game.rb
@@ -6,9 +6,12 @@ class Game
     @current_player = player1
   end
 
-  def change_player
-    @players.select { |player| player != @current_player }
-  end
+  # def change_player
+  #   # @players.select do |player|
+  #   #   return @current_player = player if @current_player != player
+  #   # end
+  #   @players.each { |player| return player if player != @current_player }
+  # end
 
   def player1
     @players.first
@@ -20,5 +23,16 @@ class Game
 
   def attack(player)
     player.reduce_points
+  end
+
+  #testing workthrough code....... trying to change players
+  def change_player
+    @current_turn = opponent_of(current_turn)
+  end
+
+  private
+
+  def opponent_of(the_player)
+    @players.select { |player| player != the_player }.first
   end
 end
